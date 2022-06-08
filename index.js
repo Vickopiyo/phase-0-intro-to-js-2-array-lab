789e53e709474274de676f771169bd0537db62a0
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-08 11:23:59 +0300

    commit tree d01ea5ced2c3d1562eaa2fe4e5f282acf92a775e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats =["Milo","Otis","Garfield"]
+// cats.push("cat")
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+// console.log(cats)
+function  destructivelyPrependCat(name){
+cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+function appendCat(name){
+    return [...cats,name];
+}
+function prependCat(name){
+    return [name,...cats]
+}
+function removeLastCat(){
+ return cats.slice(0,cats.length - 1)   
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
